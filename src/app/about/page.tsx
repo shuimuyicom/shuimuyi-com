d6dcf87729871bfeb274b0b7d193d6e2dac0a1b5
shuimuyicom <shuimuyi@example.com>
2025-08-02 23:59:44 +0800

    -文案

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -69,7 +69,7 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            我是水木易，一名充满热情的 Vibe Coder。
+            水木易：Vibe Coder；产品经理；哲学硕士；
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
